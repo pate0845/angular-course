0ea5f2aa4a421da01d3b22dffb4e57cbd755fa0d
Rutvik <pate0845@algonquinlive.com>
2023-02-08 22:25:59 -0500

    course changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
+import {
+  AfterViewInit,
+  Component,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
+import { COURSES } from "../db-data";
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { Course } from "./model/course";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  courses = COURSES;
 
+  startDate = new Date(2000, 0, 1);
+  title = COURSES[0].description;
+  price = 99.99;
 
+  @ViewChildren("course")
+  card: QueryList<CourseCardComponent>;
 
+  onCourseSelected(course: Course) {
+    console.log(this.card);
+  }
+
+  ngAfterViewInit(): void {
+    this.onchange();
+    this.card.changes.subscribe();
+
+  }
+
+  onchange(){
+    this.courses.push(
+      {
+        id: 2,
+        description: "RxJs In Practice Course",
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png',
+        longDescription: "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
+        category: 'BEGINNER',
+        lessonsCount: 10
+    }
+    )
+  }
 }
--- src/db-data.ts
+++ src/db-data.ts
@@ -4,8 +4,7 @@ export const COURSES: any = [
 
     {
         id: 1,
-        description: "Angular Core Deep Dive",
-        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        description: "Angular Core deep dive",
         longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
         category: 'INTERMEDIATE',
         lessonsCount: 10
